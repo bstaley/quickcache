c9cc3963c7663274cc9bc2febc9136124c74ecbd
Brandon R Staley <bstaley0@live.com>
2015-06-19 12:29:13 -0400

    Refactoring, added a new error callback and changed the comments over.
    This is a large change, mostly because the file is so small. There was
    no place to catch errors of QuotaExceeded and no place to bubble up the
    error. Now we have included a callback which can be used to handle any
    errors coming out of this module. We also switched from using jsdoc to
    visual studio xml comments. Xml comments seem to have better support of
    method overloads and plus I'm 98% visual studio dev.

--- quickCache.js
+++ quickCache.js
@@ -1,7 +1,7 @@
 ﻿/*
 *Check out my site site here: https://github.com/bstaley/
 *
-*   Copyright 2014 Brandon R Staley
+*   Copyright 2015 Brandon R Staley
 *
 *   Licensed under the Apache License, Version 2.0 (the "License");
 *   you may not use this file except in compliance with the License.
@@ -23,62 +23,94 @@
 *Contributers:
 */
 
-/*
- * script for storing json objects in the localstorage, if available. has a timeout feature that will ignore expired data.
- * @param {int} [timeout] - time in milliseconds that the cached data last before expiring.
- */
-var QuickCache = function (timeout) {
+var QuickCache = function (timeout, errorCallback) {
+    /// <signature>
+    /// <summary>script for storing json objects in the localstorage, if available. has a timeout feature that will ignore expired data.</summary>
+    /// </signature>
+    /// <signature>
+    /// <summary>script for storing json objects in the localstorage, if available. has a timeout feature that will ignore expired data.</summary>
+    /// <param name="timeout" type="int">time in milliseconds that the cached data last before expiring.</param>
+    /// </signature>
+    /// <signature>
+    /// <summary>script for storing json objects in the localstorage, if available. has a timeout feature that will ignore expired data.</summary>
+    /// <param name="errorCallback" type="function(error)">a callback function that will handle bubbling the error to your assigned function</param>
+    /// </signature>
+    /// <signature>
+    /// <summary>script for storing json objects in the localstorage, if available. has a timeout feature that will ignore expired data.</summary>
+    /// <param name="timeout" type="int">time in milliseconds that the cached data last before expiring.</param>
+    /// <param name="errorCallback" type="function(error)">a callback function that will handle bubbling the error to your assigned function</param>
+    /// </signature>
 
     //keep context
     var cache = this;
 
+    //check overloads 
+    if (timeout && typeof (timeout) === 'function') {
+        errorCallback = timeout;
+        timeout = null;
+    }
+
     //store the timeout amount for measurement
     cache.timeout = timeout;
 
-
-    /*
-     * check to see if localstorage is allowed
-     */
-    cache.supportsLocalStorage = function () {
+    //check to see if localstorage is allowed
+    cache.isSupported = (function () {
         try {
+            //check to see if localStorage exist. if it does it will throw an exception
             return 'localStorage' in window && window['localStorage'] !== null;
         } catch (e) {
-            return false;
+            //by not returning anything undefined will be the product of this function
+            handleError('localStorage is not supported');
         }
-    }
+    })();
 
-    /*
-     * store a javascript object in the localStorage
-     * @param {string} key - the unique identifier to store an object against.
-     * @param {object} data - any javascript object to store.
-     */
     cache.set = function (key, data) {
+        /// <summary>store a javascript object in the localStorage.</summary>
+        /// <param name="key" type="string">the unique identifier to store an object against.</param>
+        /// <param name="data" type="object">any javascript object to store.</param>
+        /// <returns type="object">why not return the data you put in. chaining and stuff.</returns>
+
         //check localStorage availability
-        if (cache.supportsLocalStorage()) {
-            //since timeout is optional we may not need to set a timeout
-            if (cache.timeout) {
-                //this will store an associated time with a key so that we can check to see if the data is expired
-                localStorage.setItem(key + 'Time', new Date().toJSON());
+        if (cache.isSupported) {
+            try {
+                //since timeout is optional we may not need to set a timeout
+                if (cache.timeout) {
+                    //this will store an associated time with a key so that we can check to see if the data is expired
+                    localStorage.setItem(key + 'Time', new Date().toJSON());
+                }
+                //stringify and store
+                localStorage.setItem(key, JSON.stringify(data));
+                //why not return the data you put in. chaining and stuff.
+                return data;
             }
-            //stringify and store
-            localStorage.setItem(key, JSON.stringify(data));
+            catch (ex) {
+                handleError(ex.message);
+            }
         }
-        else { console.log("Can't Cache"); }
     };
 
-    /*
-    * retreive a javascript object from the localStorage
-    * @param {string} key - the unique identifier that an object is stored with.
-    */
     cache.get = function (key) {
+        /// <summary>retreive a javascript object from the localStorage.</summary>
+        /// <param name="key" type="string">the unique identifier to store an object against.</param>
+        /// <returns type="object">will return null if timeout expired/cache isn't supported/object wasn't found. otherwise it will return the found object.</returns>
+
         //check localStorage availability
-        if (cache.supportsLocalStorage()) {
+        if (cache.isSupported) {
             //if timeout isn't defined just get the data, if it is check the timeout against time now to see if it is expired
             if (!cache.timeout || (new Date() - new Date(localStorage.getItem(key + 'Time'))) < cache.timeout) {
                 //if not expired return the json pared data.
                 return JSON.parse(localStorage.getItem(key));
             }
         }
-        else { console.log("Can't Cache"); }
+        //return what you would expect if we could not find the value
+        return null;
     };
+
+    function handleError(errMessage) {
+        /// <summary>private method to handle errors. makes the callback and console logs.</summary>
+        /// <param name="errMessage" type="string">the message from the error.</param>
+        var error = new Error(errMessage);
+        !errorCallback || errorCallback.apply(cache, [error]);
+        console.log(error);
+    }
 };
